48eac127b63ca59de5542fa29b33b404e269bf84
Adrian Stanek <stanek@bitsinmotion.de>
2023-08-11 01:47:17 +0200

    update: updatespec.sh with waiter, and active Deployment display

--- scripts/updatespec.sh
+++ scripts/updatespec.sh
@@ -19,6 +19,7 @@ doctl auth switch --context "$authContext"
 # Get actual App ID
 appId=$(getAppId "$appName")
 echo "App ID: $appId"
+echo "Spec: ./$specFolder/$appName.$extension"
 
 if [[ ! $appId =~ ^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$ ]]; then
     echo "App ID could not be found!"
@@ -27,32 +28,78 @@ fi
 
 # Update the Spec
 # shellcheck disable=SC2086
-doctl apps update $appId --spec "./$specFolder/$appName.$extension" --format ID,Spec.Name
-output=$(doctl apps get "$appId")
-deployment_id=$(echo "$output" | awk 'NR==2 {print $4}')
 
-# 10 Minutes timeout
-timeout=600
-counter=0
+doctl apps update $appId --spec "./$specFolder/$appName.$extension" --format ID --no-header
 
-echo "Waiting for Deployment Result of: $deployment_id ...";
+attempts=0
+max_attempts=10
+interval=1
 
-# Check for either success, error or timeout
-while true; do
-    # Run your command and capture the output
-    output=$(doctl apps get-deployment $appId $deployment_id)
+while [[ $attempts -lt $max_attempts ]]; do
+    output=$(doctl apps get $appId --format InProgressDeployment.ID)
+    active_deployment_id=$(echo "$output" | awk 'NR==2 {print $1}')
 
-    # Check if the output contains "5/5" or "error"
-    if [[ $output == *"5/5"* ]]; then
-        echo -e "\n##########\nSUCCESS:\n $output"
-
-        # Down the current app spec to update for example secrets
-        ./scripts/getspec.sh $authContext $appName;
-        exit 0;
+    # If we find the active_deployment_id, break out of the loop
+    if [[ ! -z "$active_deployment_id" ]]; then
         break
     fi
 
-    # Check if the output contains "5/5" or "error"
+    # Increment the attempts and sleep
+    ((attempts++))
+    sleep $interval
+done
+
+# Optionally, check if we found the active_deployment_id after max_attempts
+if [[ -z "$active_deployment_id" ]]; then
+    echo "Failed to fetch active deployment ID after $max_attempts attempts."
+    exit 1
+fi
+
+active_deployment_id=$(echo "$output" | awk 'NR==2 {print $1}')
+
+echo "Active Deployment ID $active_deployment_id";
+
+symbols=("[.  ]" "[ . ]" "[  .]")
+# 10 Minutes timeout
+timeout=600
+counter=0
+echo "Waiting for Deployment Result of: $active_deployment_id ...";
+
+# Check for either success, error or timeout
+while true; do
+
+    # Run your command and capture the output
+    output=$(doctl apps get-deployment $appId $active_deployment_id --no-header --format ID,Cause,Progress,Updated)
+
+    # Extract current progress and total from the string "x/y", where x is the current progress and y is the total
+    progress=$(doctl apps get-deployment $appId $active_deployment_id --format Progress)
+    current_progress=${progress%/*}  # Extracts the number before the slash
+    total=${progress#*/}            # Extracts the number after the slash
+
+   # Ensure the values are numeric by removing all non-digit characters
+   clean_current_progress=${current_progress//[^0-9]/}
+   clean_total=${total//[^0-9]/}
+
+   # Ensure we didn't get empty values after cleaning; if we did, there's likely an issue with the inputs
+   if [[ -z "$clean_current_progress" || -z "$clean_total" ]]; then
+       echo "Error: Invalid values retrieved for progress. Current Progress: '$current_progress', Total: '$total'"
+       exit 1
+   fi
+
+   # Now, compare the sanitized values
+   if [[ $clean_current_progress -ge $clean_total ]]; then
+       echo -e "\nUpdate successful!\n"
+
+       echo -e "Retrieving updated App Spec ..."
+
+       # Download the current app spec to update for example secrets
+       ./scripts/getspec.sh $authContext $appName
+
+        echo -e "\nDone!\n"
+       exit 0
+   fi
+
+    # Check if the output contains "error"
     if [[ $output == *"error"* ]]; then
         echo -e "\n##########\nERROR:\n $output"
         break
@@ -63,11 +110,15 @@ while true; do
 
     # If the counter reaches the timeout value, exit the loop
     if [[ $counter -ge $timeout ]]; then
-        echo "Timeout after 10 minutes."
+        echo "\nTimeout after 10 minutes."
         break
     fi
+
+    index=$((counter % 3))
+
+    # Print the updated output on the same line using carriage return
+    echo -ne "$output ${symbols[$index]} \r";
 
     # Wait for one second before the next iteration
     sleep 1
 done
-
